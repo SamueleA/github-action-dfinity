ced637fbefc08717e29d12a70b109dd35ec4e0f8
sagostinelli <batonenbois@gmail.com>
2021-03-24 12:00:22 -0400

    script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
+echo here is the PK
 
-yes | sh -ci "$(curl -fsSL https://sdk.dfinity.org/install.sh)" | killall -9 yes
+echo $INPUT_PRIVATEKEY
 
-echo $INPUT_APIKEY
-
-dfx deploy --network=ic
+# dfx deploy --network=ic
