408433ac5e3419435a3fe5cfc28d90ff9a624e3e
sagostinelli <batonenbois@gmail.com>
2021-03-24 16:19:44 -0400

    new deploy

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,11 +11,7 @@ echo Adding identity.pem and wallets file file
 
 echo $INPUT_IDENTITY > ~/.config/dfx/identity/default/identity.pem
 sed -i 's/\\r\\n/\r\n/g' ~/.config/dfx/identity/default/identity.pem
-# echo $INPUT_WALLETS > ~/.config/dfx/identity/default/wallets.json
-
-echo "Fetching wallet from network"
-dfx identity --network=ic get-wallet
-
+echo $INPUT_WALLETS > ~/.config/dfx/identity/default/wallets.json
 
 echo "Deploying to the IC"
 dfx deploy --network=ic
