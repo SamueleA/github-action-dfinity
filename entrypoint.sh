b911db996154951a0b4eb105b74f1d69d39282e6
sagostinelli <batonenbois@gmail.com>
2021-03-24 15:28:38 -0400

    pem new tet

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,12 +10,10 @@ mkdir ~/.config/dfx/identity/default
 echo Adding identity.pem and wallets file file
 
 echo $INPUT_IDENTITY > ~/.config/dfx/identity/default/identity.pem
+sed -i 's/\\r\\n/\r\n/g' ~/.config/dfx/identity/default/identity.pem
+echo $INPUT_WALLETS > ~/.config/dfx/identity/default/wallets.json
 
 echo $(ls ~/.config/dfx/identity/default)
-
-echo $(cat ~/.config/dfx/identity/default/identity.pem) | sed 's/./&-/g'
-
-dfx identity --network=ic get-wallet
-# echo $INPUT_WALLETS > ~/.config/dfx/identity/default/wallets.json
+echo $(ls ~/.config/dfx/identity/default/identity.pem)
 
 dfx deploy --network=ic
