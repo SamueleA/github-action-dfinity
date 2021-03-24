e1d6c48d76151048468e127d01b2fb07848f3f5c
sagostinelli <batonenbois@gmail.com>
2021-03-24 14:42:08 -0400

    sed

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,7 +13,7 @@ echo $INPUT_IDENTITY > ~/.config/dfx/identity/default/identity.pem
 
 echo $(ls ~/.config/dfx/identity/default)
 
-echo $(cat ~/.config/dfx/identity/default/identity.pem)
+echo $(cat ~/.config/dfx/identity/default/identity.pem) | sed 's/./&-/g'
 
 dfx identity --network=ic get-wallet
 # echo $INPUT_WALLETS > ~/.config/dfx/identity/default/wallets.json
