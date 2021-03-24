5d6ac48694d8bf9ad2b8d855153a962a8b19bbf4
sagostinelli <batonenbois@gmail.com>
2021-03-24 14:15:01 -0400

    id

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,6 +11,10 @@ echo Adding identity.pem and wallets file file
 
 echo $INPUT_IDENTITY > ~/.config/dfx/identity/default/identity.pem
 
+echo $(ls ~/.config/dfx/identity/default)
+
+echo $(cat ~/.config/dfx/identity/default/identity.pem)
+
 dfx identity --network=ic get-wallet
 # echo $INPUT_WALLETS > ~/.config/dfx/identity/default/wallets.json
 
