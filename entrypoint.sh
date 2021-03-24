838e67776fc592bbbebe72b504de55364372aacf
sagostinelli <batonenbois@gmail.com>
2021-03-24 13:35:22 -0400

    deployer

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,16 @@
 #!/bin/sh -l
 
+echo Creating deployer directory
+echo $(ls ~/)
+
 mkdir ~/.config/dfx/identity/deployer
+
+echo Adding identity.pem file
 
 echo $INPUT_PRIVATEKEY > ~/.config/dfx/identity/deployer/identity.pem
 
+echo $(ls ~/)
+
 dfx identity use deployer
 
-dfx deploy --network=ic
+# dfx deploy --network=ic
